786ee4921a013e294a3e17ac11f3dc8b1272a107
Martin Kopeček <github@smallhill.cz>
2021-01-07 13:42:04 +0100

    remove repeating code

--- katalog/src/main.ts
+++ katalog/src/main.ts
@@ -50,12 +50,22 @@ async function fetchEntities(): Promise<Entity[]> {
       const mime = sr.mimetype || lookup(sr.format?.toLowerCase());
       if (mime) typ_média = "http://www.iana.org/assignments/media-types/" + mime;
 
+      const podmínky_užití = {
+        typ: "Specifikace podmínek užití" as "Specifikace podmínek užití",
+        autorské_dílo: sr.license_autorske_dilo,
+        databáze_chráněná_zvláštními_právy: sr.license_zvlastni_prava_databaze,
+        databáze_jako_autorské_dílo: sr.license_originalni_databaze,
+        osobní_údaje: sr.license_osobni_udaje
+      };
+
+      const název = { "cs": sr.title };
+
       if (sr.service_endpointURL || sr.service_endpointDescription) {
 
         const service: DatovaSluzba & { iri: string } = {
           iri: iri + "/sluzba",
           typ: "Datová služba",
-          název: { "cs": sr.title },
+          název,
           přístupový_bod: fixUrl(sr.service_endpointURL) || fixUrl(sr.service_endpointDescription),
           popis_přístupového_bodu: fixUrl(sr.service_endpointDescription)
         };
@@ -63,14 +73,8 @@ async function fetchEntities(): Promise<Entity[]> {
         const distribution: DistribuceSluzba = {
           iri,
           typ: "Distribuce",
-          název: { "cs": sr.title },
-          podmínky_užití: {
-            typ: "Specifikace podmínek užití",
-            autorské_dílo: sr.license_autorske_dilo,
-            databáze_chráněná_zvláštními_právy: sr.license_zvlastni_prava_databaze,
-            databáze_jako_autorské_dílo: sr.license_originalni_databaze,
-            osobní_údaje: sr.license_osobni_udaje
-          },
+          název,
+          podmínky_užití,
           přístupové_url: fixUrl(sr.accessURL) || fixUrl(sr.service_endpointURL) || fixUrl(sr.service_endpointDescription),
           přístupová_služba: service
         };
@@ -83,16 +87,10 @@ async function fetchEntities(): Promise<Entity[]> {
         const distribution: DistribuceSoubor = {
           iri,
           typ: "Distribuce",
-          název: { "cs": sr.title },
+          název,
           formát: "http://publications.europa.eu/resource/authority/file-type/" + sr.format?.toUpperCase(),
           typ_média,
-          podmínky_užití: {
-            typ: "Specifikace podmínek užití",
-            autorské_dílo: sr.license_autorske_dilo,
-            databáze_chráněná_zvláštními_právy: sr.license_zvlastni_prava_databaze,
-            databáze_jako_autorské_dílo: sr.license_originalni_databaze,
-            osobní_údaje: sr.license_osobni_udaje
-          },
+          podmínky_užití,
           soubor_ke_stažení: fixUrl(sr.downloadURL),
           přístupové_url: fixUrl(sr.accessURL)
         };
