a4ae06925003e915b760bc6c15f2441335424a9d
APmf16211 <95355210+APmf16211@users.noreply.github.com>
2022-02-03 14:43:12 +0100

    Update resource.ts

--- katalog/src/controllers/resource.ts
+++ katalog/src/controllers/resource.ts
@@ -60,7 +60,7 @@ export async function getResource(id: string) {
       podmínky_užití,
       soubor_ke_stažení: fixUrl(sr.downloadURL),
       přístupové_url: fixUrl(sr.accessURL),
-      schéma: fixUrl(sr.schemaURL)
+      schéma: sr.sch_ma
     };
 
     return distribution;
